3a91dfb03ac8cdfb69940b54254b162283f664ca
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-10-03 17:38:52 -0500

    Update: Add buttons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,6 @@
 return {
 	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local startify = require("alpha.themes.startify")
 
@@ -17,7 +17,15 @@ return {
 		}
 
 		startify.file_icons.provider = "devicons"
-
+		startify.section.top_buttons.val = {
+			startify.button("e", "  New file", ":ene <CR>"),
+			startify.button("SPC f f", "󰈞  Find file", ":Telescope find_files<CR>"),
+			startify.button("SPC f h", "  Recently opened files", ":Telescope oldfiles<CR>"),
+			startify.button("SPC f r", "󰃃  Frecency/MRU", ":Telescope frecency<CR>"),
+			startify.button("SPC f g", "  Find word", ":Telescope live_grep<CR>"),
+			startify.button("SPC f m", "  Jump to bookmarks", ":Telescope marks<CR>"),
+			startify.button("SPC s l", "󰑤  Open last session", ":SessionLoad<CR>"),
+		}
 		require("alpha").setup(startify.config)
 	end,
 }
